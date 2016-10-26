c68dda8e4783821107063deab39685061047d90a
Ethan He <tashaxing123@163.com>
2016-10-26 14:35:57 +0800

    add

---
+++ My3DTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My3DTouch
+//
+//  Created by yxhe on 16/10/26.
+//  Copyright © 2016年 tashaxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
